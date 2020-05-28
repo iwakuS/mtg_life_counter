66061506ca519f8ff33ba26d47cabef8cc8722c4
iwakuS <iwakura.shingo@jp.fujitsu.com>
2020-05-28 18:20:11 +0900

    Module分け（Screenの中で表示させるウィジェット）

---
+++ lib/module/LifeCounter.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LifeCounter extends StatefulWidget {
+  LifeCounter({Key key, this.name}) : super(key: key);
+  final String name;
+
+  @override
+  _LifeCounterState createState() => _LifeCounterState();
+}
+
+class _LifeCounterState extends State<LifeCounter> {
+  // 画面を横に固定する場合
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
+  int _counter = 20;
+
+  void _changeCounter(int number) {
+    setState(() {
+      _counter += number;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 20;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _resetCounter,
+              tooltip: 'Reset',
+              child: Icon(Icons.update),
+            ),
+            Text(
+              widget.name,
+            ),
+            FlatButton(
+              shape: CircleBorder(),
+              child: Text(
+                '$_counter',
+                style: TextStyle(fontSize: 70),
+              ),
+              onPressed: () => _changeCounter(-1),
+              highlightColor: Colors.pink,
+              splashColor: Colors.red,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.exposure_neg_1),
+                tooltip: 'Decrease count by 1',
+                onPressed: () => _changeCounter(-1),
+                highlightColor: Colors.pink,
+                splashColor: Colors.red,
+              ),
+              const SizedBox(width: 20.0),
+              IconButton(
+                icon: Icon(Icons.exposure_plus_1),
+                tooltip: 'Increase count by 1',
+                onPressed: () => _changeCounter(1),
+                highlightColor: Colors.pink,
+                splashColor: Colors.blue,
+              ),
+            ]),
+            const SizedBox(height: 20.0),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.exposure_neg_2),
+                tooltip: 'Decrease count by 2',
+                onPressed: () => _changeCounter(-2),
+                highlightColor: Colors.pink,
+                splashColor: Colors.orange,
+              ),
+              const SizedBox(width: 20.0),
+              IconButton(
+                icon: Icon(Icons.exposure_plus_2),
+                tooltip: 'Increase count by 2',
+                onPressed: () => _changeCounter(2),
+                highlightColor: Colors.pink,
+                splashColor: Colors.indigo,
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/life_counter_screen.dart
+++ lib/screens/life_counter_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:mtglifecounter/module/LifeCounter.dart';
 
 class LifeCounterScreen extends StatelessWidget {
   const LifeCounterScreen();
@@ -20,10 +20,10 @@ class LifeCounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counters = <Counter>[];
+    final counters = <LifeCounter>[];
 
     for (var i = 0; i < _playerNames.length; i++) {
-      counters.add(Counter(
+      counters.add(LifeCounter(
         name: _playerNames[i],
       ));
     }
@@ -35,112 +35,6 @@ class LifeCounterScreen extends StatelessWidget {
 
     return Scaffold(
       body: gridView,
-    );
-  }
-}
-// ##### Finish:CounterRoute.dart#####
-// #########################################################################
-
-// #########################################################################
-// ##### Start:Counter.dart#####
-//import 'package:flutter/material.dart';
-
-class Counter extends StatefulWidget {
-  Counter({Key key, this.name}) : super(key: key);
-  final String name;
-
-  @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  // 画面を横に固定する場合
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.landscapeLeft,
-    ]);
-  }
-
-  int _counter = 20;
-
-  void _changeCounter(int number) {
-    setState(() {
-      _counter += number;
-    });
-  }
-
-  void _resetCounter() {
-    setState(() {
-      _counter = 20;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: _resetCounter,
-              tooltip: 'Reset',
-              child: Icon(Icons.update),
-            ),
-            Text(
-              widget.name,
-            ),
-            FlatButton(
-              shape: CircleBorder(),
-              child: Text(
-                '$_counter',
-                style: TextStyle(fontSize: 70),
-              ),
-              onPressed: () => _changeCounter(-1),
-              highlightColor: Colors.pink,
-              splashColor: Colors.red,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.exposure_neg_1),
-                tooltip: 'Decrease count by 1',
-                onPressed: () => _changeCounter(-1),
-                highlightColor: Colors.pink,
-                splashColor: Colors.red,
-              ),
-              const SizedBox(width: 20.0),
-              IconButton(
-                icon: Icon(Icons.exposure_plus_1),
-                tooltip: 'Increase count by 1',
-                onPressed: () => _changeCounter(1),
-                highlightColor: Colors.pink,
-                splashColor: Colors.blue,
-              ),
-            ]),
-            const SizedBox(height: 20.0),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.exposure_neg_2),
-                tooltip: 'Decrease count by 2',
-                onPressed: () => _changeCounter(-2),
-                highlightColor: Colors.pink,
-                splashColor: Colors.orange,
-              ),
-              const SizedBox(width: 20.0),
-              IconButton(
-                icon: Icon(Icons.exposure_plus_2),
-                tooltip: 'Increase count by 2',
-                onPressed: () => _changeCounter(2),
-                highlightColor: Colors.pink,
-                splashColor: Colors.indigo,
-              ),
-            ]),
-          ],
-        ),
-      ),
     );
   }
 }

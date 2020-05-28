147cf12a34b26c02e24fa956a7690a971d4e7085
iwakuS <iwakura.shingo@jp.fujitsu.com>
2020-05-28 18:26:18 +0900

    【軽微】不要そうなWrapをRemove

--- lib/module/life_counter.dart
+++ lib/module/life_counter.dart
@@ -36,66 +36,64 @@ class _LifeCounterState extends State<LifeCounter> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: _resetCounter,
-              tooltip: 'Reset',
-              child: Icon(Icons.update),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: _resetCounter,
+            tooltip: 'Reset',
+            child: Icon(Icons.update),
+          ),
+          Text(
+            widget.name,
+          ),
+          FlatButton(
+            shape: CircleBorder(),
+            child: Text(
+              '$_counter',
+              style: TextStyle(fontSize: 70),
             ),
-            Text(
-              widget.name,
-            ),
-            FlatButton(
-              shape: CircleBorder(),
-              child: Text(
-                '$_counter',
-                style: TextStyle(fontSize: 70),
-              ),
+            onPressed: () => _changeCounter(-1),
+            highlightColor: Colors.pink,
+            splashColor: Colors.red,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.exposure_neg_1),
+              tooltip: 'Decrease count by 1',
               onPressed: () => _changeCounter(-1),
               highlightColor: Colors.pink,
               splashColor: Colors.red,
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.exposure_neg_1),
-                tooltip: 'Decrease count by 1',
-                onPressed: () => _changeCounter(-1),
-                highlightColor: Colors.pink,
-                splashColor: Colors.red,
-              ),
-              const SizedBox(width: 20.0),
-              IconButton(
-                icon: Icon(Icons.exposure_plus_1),
-                tooltip: 'Increase count by 1',
-                onPressed: () => _changeCounter(1),
-                highlightColor: Colors.pink,
-                splashColor: Colors.blue,
-              ),
-            ]),
-            const SizedBox(height: 20.0),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.exposure_neg_2),
-                tooltip: 'Decrease count by 2',
-                onPressed: () => _changeCounter(-2),
-                highlightColor: Colors.pink,
-                splashColor: Colors.orange,
-              ),
-              const SizedBox(width: 20.0),
-              IconButton(
-                icon: Icon(Icons.exposure_plus_2),
-                tooltip: 'Increase count by 2',
-                onPressed: () => _changeCounter(2),
-                highlightColor: Colors.pink,
-                splashColor: Colors.indigo,
-              ),
-            ]),
-          ],
-        ),
+            const SizedBox(width: 20.0),
+            IconButton(
+              icon: Icon(Icons.exposure_plus_1),
+              tooltip: 'Increase count by 1',
+              onPressed: () => _changeCounter(1),
+              highlightColor: Colors.pink,
+              splashColor: Colors.blue,
+            ),
+          ]),
+          const SizedBox(height: 20.0),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.exposure_neg_2),
+              tooltip: 'Decrease count by 2',
+              onPressed: () => _changeCounter(-2),
+              highlightColor: Colors.pink,
+              splashColor: Colors.orange,
+            ),
+            const SizedBox(width: 20.0),
+            IconButton(
+              icon: Icon(Icons.exposure_plus_2),
+              tooltip: 'Increase count by 2',
+              onPressed: () => _changeCounter(2),
+              highlightColor: Colors.pink,
+              splashColor: Colors.indigo,
+            ),
+          ]),
+        ],
       ),
     );
   }
